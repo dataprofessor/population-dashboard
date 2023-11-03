f8fc2fb818c97d46428ca811faa5758e9a453fef
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-11-04 00:30:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.markdown("""
     width: calc(20% - 1rem);
     flex: 1 1 calc(20% - 1rem);
     /* text-align: center; */
-    padding: 80px 10%;
+    padding: 80px 5%;
     background-color: lightblue;
 }
 </style>
