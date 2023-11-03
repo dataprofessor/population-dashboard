2f1fa1921d7f21c39f93048b36c0f3ae6f4d6ae7
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-11-03 12:07:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,13 +8,13 @@ st.set_page_config(
   
 col = st.columns(5)
 
-with col[0]
+with col[0]:
     st.write(1)
-with col[1]
+with col[1]:
     st.write(2)
-with col[2]
+with col[2]:
     st.write(3)
-with col[3]
+with col[3]:
     st.write(4)
-with col[4]
+with col[4]:
     st.write(5)
