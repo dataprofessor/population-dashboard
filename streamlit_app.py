fdb6cbd9d8e5b6517b94e8686ac0f597326a5386
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-11-03 12:07:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,15 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded")
   
-st.columns(5)
+col = st.columns(5)
+
+with col[0]
+    st.write(1)
+with col[1]
+    st.write(2)
+with col[2]
+    st.write(3)
+with col[3]
+    st.write(4)
+with col[4]
+    st.write(5)
