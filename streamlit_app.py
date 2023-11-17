1050a219ae6fb2a390d1a9b98662276dda6056ae
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-11-17 11:07:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -135,7 +135,7 @@ with row_1_col[2]:
     df_selected_year = df_reshaped[df_reshaped.year == selected_year]
     df_selected_year_sorted = df_selected_year.sort_values(by="population", ascending=False)
     
-    st.dataframe(df_selected_year_sorted)
+    #st.dataframe(df_selected_year_sorted)
                  
 
 # Row 2
