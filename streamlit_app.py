ca92f61ab360e99f24cb18b7d206b14141b87c63
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-11-17 09:56:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import altair as alt
 
 # Page configuration
 st.set_page_config(
@@ -7,6 +8,8 @@ st.set_page_config(
     page_icon="🏂",
     layout="wide",
     initial_sidebar_state="expanded")
+
+alt.themes.enable("dark")
 
 # CSS styling
 st.markdown("""
@@ -39,26 +42,40 @@ st.markdown("""
 # Load data
 df = pd.read_csv('data/us-population-2010-2019-reshaped.csv')
 
+# Plots
+heatmap = alt.Chart(df).mark_rect().encode(
+        y=alt.Y('year:O', axis=alt.Axis(title="Year", titleFontSize=16, titlePadding=15, titleFontWeight=900, labelAngle=0)),
+        x=alt.X('states:O', axis=alt.Axis(title="States", titleFontSize=16, titlePadding=15, titleFontWeight=900)),
+        color=alt.Color('max(population):Q',
+                         legend=alt.Legend(title=" "),
+                         scale=alt.Scale(scheme="blueorange")),
+        stroke=alt.value('black'),
+        strokeWidth=alt.value(0.25),
+        #tooltip=[
+        #    alt.Tooltip('year:O', title='Year'),
+        #    alt.Tooltip('population:Q', title='Population')
+        #]
+    ).properties(width=900
+    #).configure_legend(orient='bottom', titleFontSize=16, labelFontSize=14, titlePadding=0
+    #).configure_axisX(labelFontSize=14)
+    ).configure_axis(
+    labelFontSize=12,
+    titleFontSize=12
+    )
+
 # Row 1
-row_1_col = st.columns(5)
+row_1_col = st.columns((1,4))
 
 with row_1_col[0]:
-    st.write('1')
-with row_1_col[1]:
-    st.write('2')
-with row_1_col[2]:
-    #st.write('3')
     st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
-with row_1_col[3]:
-    st.write('4')
-with row_1_col[4]:
-    st.write('5')
+with row_1_col[1]:
+    st.altair_chart(heatmap, use_container_width=True)
 
 # Row 2
-row_2_col = st.columns(2)
+#row_2_col = st.columns(2)
 
-with row_2_col[0]:
-    st.write('6')
-with row_2_col[1]:
-    st.write('7')
+#with row_2_col[0]:
+#    st.write('6')
+#with row_2_col[1]:
+#    st.write('7')
 
