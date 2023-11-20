ac34af5d8c1fd09ae7c035df32a66cd9b7bf988f
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-11-20 13:55:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -266,8 +266,8 @@ fig = px.choropleth(df, geojson=counties, locations='fips', color='unemp',
 # fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 fig.update_layout(
     template='plotly_dark',
-    plot_bgcolor=rgba(0, 0, 0, 0),
-    paper_bgcolor=rgba(0, 0, 0, 0),
+    plot_bgcolor='rgba(0, 0, 0, 0)',
+    paper_bgcolor='rgba(0, 0, 0, 0)',
     margin=dict(l=20, r=20, t=20, b=20),
 )
 
