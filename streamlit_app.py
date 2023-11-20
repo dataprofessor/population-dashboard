2f640257255281a9c49987ad45e92beede2a1a4d
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-11-20 19:01:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -305,7 +305,7 @@ with row_1_col[2]:
                  )
     with st.expander('Data source'):
         st.write('''
-            - [State population data (2010-2019)](https://www.census.gov/data/datasets/time-series/demo/popest/2010s-state-total.html) obtained from the U.S. Census Bureau.
+            - State population data (2010-2019) obtained from the [U.S. Census Bureau](https://www.census.gov/data/datasets/time-series/demo/popest/2010s-state-total.html).
             ''')
 
 
