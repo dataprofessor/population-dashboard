bf933eaf2c1733e78859c7c6a44939720fd92b90
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-11-20 14:41:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,6 +70,68 @@ st.markdown("""
 # Load data
 df_reshaped = pd.read_csv('data/us-population-2010-2019-reshaped.csv')
 
+states_abbreviation = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY",
+    "District of Columbia": "DC",
+    "American Samoa": "AS",
+    "Guam": "GU",
+    "Northern Mariana Islands": "MP",
+    "Puerto Rico": "PR",
+    "United States Minor Outlying Islands": "UM",
+    "U.S. Virgin Islands": "VI",
+}
+
+
+
 # Plots
 
 # Heatmap
@@ -158,7 +220,9 @@ with st.sidebar:
     year_list = list(df_reshaped.year.unique())
     selected_year = st.selectbox('Select a year', year_list, index=len(year_list)-1)
     df_selected_year = df_reshaped[df_reshaped.year == selected_year]
+    df_selected_year['states_code'] = [states_abbreviation[x] for x in df_selected_year.states]
     df_selected_year_sorted = df_selected_year.sort_values(by="population", ascending=False)
+    
 
 
 # Choropleth map
@@ -263,7 +327,7 @@ df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-
 #                           labels={'unemp':'unemployment rate'}
 #                          )
 
-fig = px.choropleth(df_selected_year, locations=df_selected_year.id, color=df_selected_year.population,
+fig = px.choropleth(df_selected_year, locations=df_selected_year.states_code, color=df_selected_year.population,
                            color_continuous_scale="Viridis",
                            range_color=(0, 12),
                            scope="usa",
