54c6da00f6a0f983849edfa34ea94487dcc09630
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-11-20 20:40:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -209,6 +209,7 @@ with row_1_col[0]:
 
     st.metric(label=last_state_name, value=last_state_population, delta=last_state_delta)
 
+    st.markdown('#### % State w/ migration >50,000')
     st.altair_chart(make_donut(25, 'Text', 'orange'), use_container_width=True)
 
 
