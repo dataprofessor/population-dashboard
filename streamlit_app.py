5bbefad6ac29db318038bf930eac5f13b64ad760
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-11-20 14:47:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -224,24 +224,20 @@ with st.sidebar:
     df_selected_year_sorted = df_selected_year.sort_values(by="population", ascending=False)
     
 
-
 # Choropleth map
-states = alt.topo_feature(data.us_10m.url, 'states')
+choropleth_map = px.choropleth(df_selected_year, locations='states_code', color='population', locationmode="USA-states",
+                           color_continuous_scale="Viridis",
+                           range_color=(0, max(df_selected_year.population)),
+                           scope="usa",
+                           labels={'population':'Population'}
+                          )
 
-choropleth_map = alt.Chart(states).mark_geoshape().encode(
-    color=alt.Color('population:Q', scale=alt.Scale(scheme="blueorange")),   # scale=color_scale
-    stroke=alt.value('#154360')
-).transform_lookup(
-    lookup='id',
-    from_=alt.LookupData(df_selected_year, 'id', list(df_selected_year.columns))
-).properties(
-    width=500,
-    height=300
-).project(
-    type='albersUsa'
+choropleth_map.update_layout(
+    template='plotly_dark',
+    plot_bgcolor='rgba(0, 0, 0, 0)',
+    paper_bgcolor='rgba(0, 0, 0, 0)',
+    margin=dict(l=0, r=0, t=0, b=0),
 )
-
-
 
 
 # Row 1
@@ -271,11 +267,13 @@ with row_1_col[0]:
     st.metric(label=last_state_name, value=last_state_population, delta=last_state_delta)
 
     st.altair_chart(make_donut(25, 'Text', 'orange'), use_container_width=True)
-    
+
+
 with row_1_col[1]:
     st.subheader('Annual Population Growth')
     st.altair_chart(heatmap, use_container_width=True)
-    #st.altair_chart(choropleth_map, use_container_width=True)
+    st.altair_chart(choropleth_map, use_container_width=True)
+
 
 with row_1_col[2]:
     st.subheader('Top States')
@@ -302,48 +300,5 @@ with row_1_col[2]:
             ''')
 
 
-# Row 2
-#row_2_col = st.columns(2)
-
-#with row_2_col[0]:
-#    st.write('6')
-#with row_2_col[1]:
-#    st.write('7')
 
 
-from urllib.request import urlopen
-import json
-with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
-    counties = json.load(response)
-
-import pandas as pd
-df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv",
-                   dtype={"fips": str})
-
-#fig = px.choropleth(df, geojson=counties, locations='fips', color='unemp',
-#                           color_continuous_scale="Viridis",
-#                           range_color=(0, 12),
-#                           scope="usa",
-#                           labels={'unemp':'unemployment rate'}
-#                          )
-
-fig = px.choropleth(df_selected_year, locations='states_code', color='population', locationmode="USA-states",
-                           color_continuous_scale="Viridis",
-                           range_color=(0, max(df_selected_year.population)),
-                           scope="usa",
-                           labels={'population':'Population'}
-                          )
-
-
-# fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig.update_layout(
-    template='plotly_dark',
-    plot_bgcolor='rgba(0, 0, 0, 0)',
-    paper_bgcolor='rgba(0, 0, 0, 0)',
-    margin=dict(l=0, r=0, t=0, b=0),
-)
-
-with row_1_col[1]:
-    st.plotly_chart(fig, use_container_width=True)
-
-df_selected_year
