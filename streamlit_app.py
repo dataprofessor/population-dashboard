f252d443a412298d90276c9b6eb23a3c590b54ff
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-11-21 23:02:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -105,8 +105,8 @@ heatmap = alt.Chart(df_reshaped).mark_rect().encode(
     #).configure_legend(orient='bottom', titleFontSize=16, labelFontSize=14, titlePadding=0
     #).configure_axisX(labelFontSize=14)
     ).configure_axis(
-    labelFontSize=12,
-    titleFontSize=12
+    labelFontSize=16,
+    titleFontSize=16
     )
 
 # Choropleth map
