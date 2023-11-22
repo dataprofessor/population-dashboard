da6007637d09f9c28bec21687f990689b1839686
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-11-22 10:49:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,13 +92,13 @@ with st.sidebar:
 # Plots
 
 # Heatmap
-def make_heatmap():
-    heatmap = alt.Chart(df_reshaped).mark_rect().encode(
-            y=alt.Y('year:O', axis=alt.Axis(title="Year", titleFontSize=18, titlePadding=15, titleFontWeight=900, labelAngle=0)),
-            x=alt.X('states:O', axis=alt.Axis(title="", titleFontSize=18, titlePadding=15, titleFontWeight=900)),
-            color=alt.Color('max(population):Q',
+def make_heatmap(input_df, input_x, input_y, input_color, input_color_theme):
+    heatmap = alt.Chart(input_df).mark_rect().encode(
+            y=alt.Y(f'{input_y}:O', axis=alt.Axis(title="Year", titleFontSize=18, titlePadding=15, titleFontWeight=900, labelAngle=0)),
+            x=alt.X(f'{input_x}:O', axis=alt.Axis(title="", titleFontSize=18, titlePadding=15, titleFontWeight=900)),
+            color=alt.Color(f'max({input_color}):Q',
                              legend=alt.Legend(title=" "),
-                             scale=alt.Scale(scheme=selected_color_theme)),
+                             scale=alt.Scale(scheme=input_color_theme)),
             stroke=alt.value('black'),
             strokeWidth=alt.value(0.25),
             #tooltip=[
@@ -236,7 +236,7 @@ with row_1_col[1]:
     st.plotly_chart(choropleth, use_container_width=True)
     
     st.markdown('#### Total Population')
-    heatmap = make_heatmap()
+    heatmap = make_heatmap(df_reshaped, 'year', 'states', 'population', selected_color_theme)
     st.altair_chart(heatmap, use_container_width=True)
     
 
