bce64e7b03e797303b7ddb4cd3acfbcd8ee7bc9f
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-13 23:49:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -235,7 +235,9 @@ with col[0]:
 
     migrations_col = st.columns((0.2, 1, 0.2))
     with migrations_col[1]:
+        st.write('Inbound')
         st.altair_chart(donut_chart_greater)
+        st.write('Outbound')
         st.altair_chart(donut_chart_less)
 
 with col[1]:
