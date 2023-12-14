385efbee89e225ad57bbfec7cdfd2e97b35eea92
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-14 14:56:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -180,8 +180,7 @@ def calculate_population_difference(input_df, input_year):
   selected_year_data = input_df[input_df['year'] == input_year].reset_index()
   previous_year_data = input_df[input_df['year'] == input_year - 1].reset_index()
   selected_year_data['population_difference'] = selected_year_data.population.sub(previous_year_data.population, fill_value=0)
-  selected_year_data['population_difference_absolute'] = abs(selected_year_data['population_difference'])
-  return pd.concat([selected_year_data.states, selected_year_data.id, selected_year_data.population, selected_year_data.population_difference, selected_year_data.population_difference_absolute], axis=1).sort_values(by="population_difference", ascending=False)
+  return pd.concat([selected_year_data.states, selected_year_data.id, selected_year_data.population, selected_year_data.population_difference], axis=1).sort_values(by="population_difference", ascending=False)
 
 
 #######################
