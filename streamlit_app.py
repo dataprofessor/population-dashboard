5592eaa9f65a4ae2777024f422314da0c97d2af2
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2024-01-29 08:57:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,52 +17,52 @@ alt.themes.enable("dark")
 
 #######################
 # CSS styling
-st.markdown("""
-<style>
+# st.markdown("""
+# <style>
 
-[data-testid="block-container"] {
-    padding-left: 2rem;
-    padding-right: 2rem;
-    padding-top: 1rem;
-    padding-bottom: 0rem;
-    margin-bottom: -7rem;
-}
+# [data-testid="block-container"] {
+#     padding-left: 2rem;
+#     padding-right: 2rem;
+#     padding-top: 1rem;
+#     padding-bottom: 0rem;
+#     margin-bottom: -7rem;
+# }
 
-[data-testid="stVerticalBlock"] {
-    padding-left: 0rem;
-    padding-right: 0rem;
-}
+# [data-testid="stVerticalBlock"] {
+#     padding-left: 0rem;
+#     padding-right: 0rem;
+# }
 
-[data-testid="stMetric"] {
-    background-color: #393939;
-    text-align: center;
-    padding: 15px 0;
-}
+# [data-testid="stMetric"] {
+#     background-color: #393939;
+#     text-align: center;
+#     padding: 15px 0;
+# }
 
-[data-testid="stMetricLabel"] {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
+# [data-testid="stMetricLabel"] {
+#   display: flex;
+#   justify-content: center;
+#   align-items: center;
+# }
 
-[data-testid="stMetricDeltaIcon-Up"] {
-    position: relative;
-    left: 38%;
-    -webkit-transform: translateX(-50%);
-    -ms-transform: translateX(-50%);
-    transform: translateX(-50%);
-}
+# [data-testid="stMetricDeltaIcon-Up"] {
+#     position: relative;
+#     left: 38%;
+#     -webkit-transform: translateX(-50%);
+#     -ms-transform: translateX(-50%);
+#     transform: translateX(-50%);
+# }
 
-[data-testid="stMetricDeltaIcon-Down"] {
-    position: relative;
-    left: 38%;
-    -webkit-transform: translateX(-50%);
-    -ms-transform: translateX(-50%);
-    transform: translateX(-50%);
-}
+# [data-testid="stMetricDeltaIcon-Down"] {
+#     position: relative;
+#     left: 38%;
+#     -webkit-transform: translateX(-50%);
+#     -ms-transform: translateX(-50%);
+#     transform: translateX(-50%);
+# }
 
-</style>
-""", unsafe_allow_html=True)
+# </style>
+# """, unsafe_allow_html=True)
 
 
 #######################
